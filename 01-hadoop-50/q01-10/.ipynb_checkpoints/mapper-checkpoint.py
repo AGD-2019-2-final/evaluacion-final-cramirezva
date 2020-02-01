9fd1be24a5ed703499f02ff318498c27ce4d2835
Carlos Alberto Ramírez Valdes <50960515+cramirezva@users.noreply.github.com>
2020-02-01 13:20:03 -0500

    Evaluación Final Carlos Alberto Ramírez Grupo 1

---
+++ 01-hadoop-50/q01-10/.ipynb_checkpoints/mapper-checkpoint.py
@@ -0,0 +1,17 @@
+import sys
+#
+# >>> Escriba el codigo del mapper a partir de este punto <<<
+#! /usr/bin/env python
+
+import sys
+if __name__ == "__main__":
+
+    # input comes from STDIN (standard input)
+    for line in sys.stdin:
+        line = line.strip()
+        line = line.split(',')
+
+        if len(line) >=2:
+            credit_history = line[2]
+
+            sys.stdout.write("{}\t1\n".format(credit_history))
